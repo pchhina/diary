d1c791eea33a02bb66e9a66115e04b30a61a060f
Paramjot Singh <param.chhina@gmail.com>
2020-07-14 06:58:12 -0500

    added buttons for entries, hide entries by default, styled entries

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 let entryForm = document.getElementById("entry-form");
 let entriesSection = document.getElementById("entries");
 let entryTextBox = document.getElementsByClassName("entry-textbox");
+let entriesNav = document.querySelector(".entries-nav");
+let entry = 0;
 
 entryForm.addEventListener("submit", addEntryToDOM);
 
-/* when submit button is clicked, stops the page from refreshing,
- * creates a new div element and updates it's text with entry textbox,
- * and finally appends the div to section */
+/* when submit button is clicked, 
+ * 1. stops the page from refreshing,
+ * 2. creates a new div element and updates it's text with entry textbox,
+ * 3. appends the div to section 
+ * 4. clears the entry text box*/
 function addEntryToDOM(event) {
     event.preventDefault();
     let entryDiv = document.createElement("div");
     entryDiv.classList.add("single-entry");
     entryDiv.textContent = entryTextBox[0].value;
     document.getElementById("entries").appendChild(entryDiv);
+    
+    entryTextBox[0].value = "";
+    entryDiv.style.display = "none";
+
+    let displayEntryButton = document.createElement("button");
+    displayEntryButton.textContent = entry += 1;
+    entriesNav.appendChild(displayEntryButton);
 }
 
