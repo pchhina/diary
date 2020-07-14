8d90f8d2917a4388a30f28b53f5bdcb899ac0b0f
Paramjot Singh <param.chhina@gmail.com>
2020-07-13 20:32:32 -0500

    entries are pushed to a section now

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 let entryForm = document.getElementById("entry-form");
-entryForm.addEventListener("submit", function(event) {
+let entriesSection = document.getElementById("entries");
+let entryTextBox = document.getElementsByClassName("entry-textbox");
+
+entryForm.addEventListener("submit", addEntryToDOM);
+
+/* when submit button is clicked, stops the page from refreshing,
+ * creates a new div element and updates it's text with entry textbox,
+ * and finally appends the div to section */
+function addEntryToDOM(event) {
     event.preventDefault();
-})
+    let entryDiv = document.createElement("div");
+    entryDiv.classList.add("single-entry");
+    entryDiv.textContent = entryTextBox[0].value;
+    document.getElementById("entries").appendChild(entryDiv);
+}
+
